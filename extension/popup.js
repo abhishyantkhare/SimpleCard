94ce593ba52b31bdd4a20d7ed0fc46b4d90cd1b8
Abhishyant Khare <abhishyant@gmail.com>
2020-07-03 16:32:23 -0700

    extension: added basic extension w text area

---
+++ extension/popup.js
@@ -0,0 +1,10 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+})
+
+changeColor.onclick = function(element) {
+    fetch('http://localhost:5000/card')
+}
