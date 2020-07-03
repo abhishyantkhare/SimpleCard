83d64e113f2457c2680b033d67eeda6d7b67c43c
Abhishyant Khare <abhishyant@gmail.com>
2020-07-03 16:50:58 -0700

    backend: added new_card POST endpoint
    This endpoint just logs the new_card data to stdout for now.
    It expects a json body with a `content` key that contains the
    data for the card.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/card')
 def card():
     return jsonify({'content': 'Hello from the backend!'})
+
+
+@app.route('/new_card')
+def new_card():
+    card_json = request.json
+    print('card content: {}'.format(card_json['content']))
+    return '', 200
