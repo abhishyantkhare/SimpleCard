1cc6b3a0e3df9971dd423c9f4dcdddd49dbb55c1
Abhishyant Khare <abhishyant@gmail.com>
2020-07-04 13:49:06 -0700

    extension: add front and back of card

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,19 @@
 let changeColor = document.getElementById('changeColor');
-let cardText = document.getElementById('cardText');
+let frontText = document.getElementById('frontText');
+let backText = document.getElementById('backText');
 
-chrome.storage.sync.get('color', function(data) {
+chrome.storage.sync.get('color', function (data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 })
 
-changeColor.onclick = function() {
+changeColor.onclick = function () {
     fetch('http://localhost:5000/new_card', {
         method: 'POST',
         cors: 'no-cors',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({content: cardText.value})
+        body: JSON.stringify({ front: frontText.value, back: backText.value })
     })
 }
