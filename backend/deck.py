4a0265bdd6af726dbe9f2a6d49cacdffe54411fb
Abhishyant Khare <abhishyant@gmail.com>
2020-07-03 17:57:01 -0700

    backend: added deck class
    Added a Deck class which contains a list of
    cards

---
+++ backend/deck.py
@@ -0,0 +1,10 @@
+class Deck:
+    
+    def __init__(self, cards=[]):
+        self._cards = []
+
+    def get_cards(self):
+        return self.get_cards()
+    
+    def add_card(self, _card):
+        self._cards.append(_card)
