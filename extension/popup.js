de657b6b1a6624ee384116b0eaaff68bf23bfd01
Abhishyant Khare <abhishyant@gmail.com>
2020-07-03 17:21:56 -0700

    extension: send value of text area in POST request

--- extension/popup.js
+++ extension/popup.js
@@ -1,17 +1,18 @@
 let changeColor = document.getElementById('changeColor');
+let cardText = document.getElementById('cardText');
 
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 })
 
-changeColor.onclick = function(element) {
+changeColor.onclick = function() {
     fetch('http://localhost:5000/new_card', {
         method: 'POST',
         cors: 'no-cors',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({content: 'test'})
+        body: JSON.stringify({content: cardText.value})
     })
 }
